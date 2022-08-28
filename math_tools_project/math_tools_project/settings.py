00fdc537a5f876ccd8c8cf69d4e48553f0bf08dd
Madhav Attili <94516459+saattili@users.noreply.github.com>
2022-08-28 23:50:37 +0530

    Update settings.py

--- math_tools_project/math_tools_project/settings.py
+++ math_tools_project/math_tools_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zln8p%a9_dol1u-+odwp#8#nvk)rwej6hq6=bj&c9g!-f#=m2%'
+SECRET_KEY = $GITHUB_DJANGO_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
