48c85d3bd157b0f054e23a06f8b8859a0721d6aa
saattili <attsmadhav@gmail.com>
2022-08-28 23:46:01 +0530

    Added basic logic to ge and set values to UI

--- math_tools_project/front_end/views.py
+++ math_tools_project/front_end/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
+import json
 
 def index(request):
     return render(request,'index.html')
+
+
 def prime_repeat(request):
+    if request.method == "POST":
+        res = request.POST
+        data = dict(res)
+        set_value = data['prime_num'][0]
+        print()
+        #print(data.csrfmiddlewaretoken)
+        return render(request,'prime_repeat.html',{
+            'value': set_value,
+        })
+
     return render(request,'prime_repeat.html')
 
