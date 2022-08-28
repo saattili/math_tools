38ab0f6c5cc489ca075de4bbf8fb3e1dffabc22b
saattili <attsmadhav@gmail.com>
2022-08-28 23:56:14 +0530

    Saved secret

--- math_tools_project/math_tools_project/settings.py
+++ math_tools_project/math_tools_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = $GITHUB_DJANGO_SECRET
+SECRET_KEY = '$DJANGO_SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
